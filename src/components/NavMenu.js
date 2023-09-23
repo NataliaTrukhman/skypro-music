a8bf37c2b29233ba4ac76adb389db95498288e3f
NataliaTrukhman <nataliatrukhman@gmail.com>
2023-09-23 18:05:49 +0300

    NavMenu-MenuBurger

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -4,7 +4,7 @@ import './NavMenu.css'
 
 
 function NavMenu(props){
-  const [menuBurger,setMenuBurger]= useState(true);
+  const [menuBurger,setMenuBurger]= useState(false);
   function menuBurgerClick(){
     setMenuBurger(!menuBurger)
 }
@@ -19,71 +19,31 @@ function NavMenu(props){
         <span className="burger__line"></span>
         <span className="burger__line"></span>
     </div>
-    {/* NavMenuLinks  тут меняет состоение*/} 
-    <div className={`nav__menu menu ${menuBurger ? "nav__menu_visibility" : ""}`}>
-        <ul className="menu__list">
-        <li className="menu__item">
-            <a href="#" className="menu__link">Главное</a>
-        </li>
-        <li className="menu__item">
-            <a href="#" className="menu__link">Мой плейлист</a>
-        </li>
-        <li className="menu__item">
-            <a href="../signin.html" className="menu__link">Войти</a>
-        </li>
-        </ul>
-    </div>
-        
- </nav>
-    );
+    {/*  тут меняет состоение*/} 
+    {
+      menuBurger && <MenuBurger />
+     }
+</nav>
+      );
 }
-
-
 export default NavMenu
 
 
 
-
-// function NavMenu(props){
-//     return(
-//         <nav className="main__nav nav">
-//         <div className="nav__logo logo">
-//           <img className="logo__image" src="../img/logo.png" alt="logo" />
-//         </div>
-//         <BurgerMenu/>
-//         <NavMenuLinks/>
-        
-//       </nav>
-//     );
-// }
-
-
-// function BurgerMenu(){
-//   const [menuBurger,setMenuBurger]= useState(false);
-
-//     return(
-//         <div className="nav__burger burger">
-//         <span className="burger__line"></span>
-//         <span className="burger__line"></span>
-//         <span className="burger__line"></span>
-//       </div>
-//     )
-// }
-
-// function NavMenuLinks(){
-//     return(
-//         <div className="nav__menu menu">
-//         <ul className="menu__list">
-//           <li className="menu__item">
-//             <a href="#" className="menu__link">Главное</a>
-//           </li>
-//           <li className="menu__item">
-//             <a href="#" className="menu__link">Мой плейлист</a>
-//           </li>
-//           <li className="menu__item">
-//             <a href="../signin.html" className="menu__link">Войти</a>
-//           </li>
-//         </ul>
-//       </div>
-//     )
-// }
+function MenuBurger(){
+    return(
+        <div className="nav__menu menu">
+        <ul className="menu__list">
+          <li className="menu__item">
+            <a href="#" className="menu__link">Главное</a>
+          </li>
+          <li className="menu__item">
+            <a href="#" className="menu__link">Мой плейлист</a>
+          </li>
+          <li className="menu__item">
+            <a href="../signin.html" className="menu__link">Войти</a>
+          </li>
+        </ul>
+      </div>
+    )
+}
