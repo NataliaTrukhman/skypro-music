985bef5bdebfd03a9e09a1c728e23f2a2c1a33e1
NataliaTrukhman <nataliatrukhman@gmail.com>
2023-10-18 20:01:55 +0300

    Skeleton

--- src/components/BarAudioPlayer.js
+++ src/components/BarAudioPlayer.js
@@ -38,38 +38,22 @@ import './BarAudioPlayer.css';
                       </svg>
                     </div>
                   </div>
-
                   <div className="player__track-play track-play">
-                    <div className="track-play__contain">
-                      <div className="track-play__image">
-                        <svg className="track-play__svg" alt="music">
-                          <use xlinkHref="./img/icon/sprite.svg#icon-note"></use>
-                        </svg>
-                      </div>
-                      <div className="track-play__author">
-                        <a className="track-play__author-link" href="/#"
-                          >Ты та...</a
-                        >
-                      </div>
-                      <div className="track-play__album">
-                        <a className="track-play__album-link" href="/#">Баста</a>
-                      </div>
-                    </div>
-
-                    <div className="track-play__like-dis">
-                      <div className="track-play__like _btn-icon">
-                        <svg className="track-play__like-svg" alt="like">
-                          <use xlinkHref="./img/icon/sprite.svg#icon-like"></use>
-                        </svg>
-                      </div>
-                      <div className="track-play__dislike _btn-icon">
-                        <svg className="track-play__dislike-svg" alt="dislike">
-                          <use
-                            xlinkHref="./img/icon/sprite.svg#icon-dislike"
-                          ></use>
-                        </svg>
-                      </div>
-                    </div>
+                     <BarContainLoad/>
+                     <div className="track-play__like-dis">
+                        <div className="track-play__like _btn-icon">
+                          <svg className="track-play__like-svg" alt="like">
+                            <use xlinkHref="./img/icon/sprite.svg#icon-like"></use>
+                          </svg>
+                        </div>
+                        <div className="track-play__dislike _btn-icon">
+                          <svg className="track-play__dislike-svg" alt="dislike">
+                            <use
+                              xlinkHref="./img/icon/sprite.svg#icon-dislike"
+                            ></use>
+                          </svg>
+                        </div>
+                     </div>
                   </div>
                 </div>
                 <div className="bar__volume-block volume">
@@ -97,3 +81,23 @@ import './BarAudioPlayer.css';
   }
 
 export default BarAudioPlayer
+
+function BarContainLoad(){
+  return (
+    <div className="track-play__contain">
+    <div className="track-play__image">
+      <svg className="track-play__svg" alt="music">
+        <use xlinkHref="./img/icon/sprite.svg#icon-note"></use>
+      </svg>
+    </div>
+    <div className="track-play__author">
+      <a className="track-play__author-link" href="#"
+        >Ты та...
+        </a>
+    </div>
+    <div className="track-play__album">
+      <a className="track-play__album-link" href="#">Баста</a>
+    </div>
+  </div>
+  )
+}
